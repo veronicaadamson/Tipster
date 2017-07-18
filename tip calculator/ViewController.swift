7b37c3d2e591240f20464efbc6116774935b7711
Veronica Adamson <veronicaadamson@Veronicas-MacBook-Pro.local>
2017-07-17 19:48:48 -0700

    Initial Commit

---
+++ tip calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip calculator
+//
+//  Created by Veronica Adamson on 7/17/17.
+//  Copyright © 2017 Streetcode Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

3212f940209e9486769a79dbf0e6f538993d8fa8
Veronica Adamson <veronicaadamson@Veronicas-MacBook-Pro.local>
2017-07-19 20:29:55 -0700

    start coding

--- tip calculator/ViewController.swift
+++ tip calculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var totalAmountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,12 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
+    @IBAction func calcTip(_ sender: UITextField) {
+    }
+    
 }
 
